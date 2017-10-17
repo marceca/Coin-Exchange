5b7f467f4109b944f524ad2ffe1b35cd84a54229
Mike <mmarceca@aardvarkbrigade.com>
2017-10-17 13:20:02 -0400

    added a comment and got a start on kraken api

--- markets/kraken/kraken_ticker.php
+++ markets/kraken/kraken_ticker.php
@@ -1,10 +1,12 @@
 <?php 
+// Use https://www.kraken.com/help/api#get-ticker-info
+// to finish up Kraken. WIP
+
 
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.kraken.com/0/public/Ticker",
-  'PAIR' => "a, b, c",
+  CURLOPT_URL => "https://api.kraken.com/0/public/AssetPairs",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
@@ -18,9 +20,21 @@ $response = curl_exec($curl);
 $err = curl_error($curl);
 
 curl_close($curl);
-var_dump($response); exit;
 
 $response = json_decode($response, true);
 
+$markets = array();
+$i = 0;
+print_r('<pre>');
+var_dump($response);exit;
+foreach ($response as $value => $coin) {
+	var_dump($coin['BCHEUR']);
+	$i++;
+}
+
+exit;
+
+print_r('<pre>');
+var_dump($response); exit;
 echo "<pre>"; print_r($response);
 ?>
